f73c9891431b0d53334a1ac3f6ddd9e93934aad7
emiliaduzac <eduzac@fi.uba.ar>
2025-08-26 07:41:54 -0300

    script done

---
+++ generar-compose.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# base variables in case no args are given
+OUTFILE="docker-compose-2-dev.yaml"
+NCLIENTS=1
+
+# if 2 args are given, override the defaults
+if [ $# -eq 2 ]; then
+  OUTFILE="$1"
+  NCLIENTS="$2"
+else
+  echo "Using defaults: "$OUTFILE" with "$NCLIENTS" clients"
+fi
+
+# use the base compose
+cat docker-compose-base.yaml > "$OUTFILE"
+
+# add the clients
+for i in $(seq 1 "$NCLIENTS"); do
+cat <<EOF >> "$OUTFILE" 
+  client$i:
+    container_name: client$i
+    image: client:latest
+    entrypoint: /client
+    environment:
+      - CLI_ID=$i
+      - CLI_LOG_LEVEL=DEBUG
+    networks:
+      - testing_net
+    depends_on:
+      - server
+
+EOF
+done
+
+# add the network config
+cat <<EOF >> "$OUTFILE"
+networks:
+  testing_net:
+    ipam:
+      driver: default
+      config:
+        - subnet: 172.25.125.0/24
+
+EOF
+
+echo "Generated "$OUTFILE" with "$NCLIENTS" clients"
