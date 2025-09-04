c14ccd88559179921d6a93a2c20eeaccac247132
emiliaduzac <eduzac@fi.uba.ar>
2025-09-03 23:14:49 -0300

    delete unnecesary exit

--- server/main.py
+++ server/main.py
@@ -53,7 +53,6 @@ def main():
     # Initialize server and start server loop
     server = Server(port, listen_backlog, total_clients)
     server.run()
-    exit(0)
 
 def initialize_log(logging_level):
     """
